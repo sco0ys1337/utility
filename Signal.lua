6a3784926c767733f69f27ebc98ccc76d200b003
vozoid <62110949+vozoid@users.noreply.github.com>
2022-07-21 15:15:18 +0500

    Update Signal.lua

--- Signal.lua
+++ Signal.lua
@@ -1,129 +1,63 @@
--- made by vozoid B)
+local newInstance = Instance.new
+local setmetatable = setmetatable
 
 local Signal = {}
 local Connection = {}
 
-do
-    -- localization
-    local instance_new = Instance.new
-    local coroutine_wrap = coroutine.wrap
-    local table_clear = table.clear
-    local setmetatable = setmetatable
-    local table_remove = table.remove
+Signal.__index = Signal
+Connection.__index = Connection
 
-    Signal.__index = Signal
-    Connection.__index = Connection
+function Connection.new(signal, callback)
+    local connection = signal._signal:Connect(callback)
 
-    --[[
-        Creates a new connection
-        @return Connection (table)
-    ]]
+    signal._count += 1
+    signal.Active = true
 
-    function Connection.new(event, callback)
-        local self = setmetatable({}, Connection)
-
-        self.Connected = true
-        self._connection = event._event:Connect(callback)
-        self._event = event
-
-        event._connections[self._connection] = true
-
-        return self
-    end
-
-    --[[
-        Disconnects a connection
-    ]]
-
-    function Connection:Disconnect()
-        self._event._connections[self._connection] = nil
-        self._event._count -= 1
-
-        self.Connected = false
-
-        self._connection:Disconnect()
-    end
-
-    --[[
-        Creates a new signal
-        @return Signal (table)
-    ]]
-
-    function Signal.new()
-        local self = setmetatable({}, Signal)
-
-        self._bindable_event = instance_new("BindableEvent")
-        self._event = self._bindable_event.Event
-        self._count = 0
-        self._connections = {}
-
-        return self
-    end
-
-    --[[
-        Fires the connections with the argument ...
-    ]]
-
-    function Signal:Fire(...)
-         self._bindable_event:Fire(...)
-    end
-
-    --[[
-        Create a new connection. Returns a connection which can be disconnected. Callback called when :Fire(...) is called
-        @return Connection (table)
-    ]]
-
-    function Signal:Connect(callback)
-        local connection = Connection.new(self, callback)
-        self._count += 1
-        return connection
-    end
-
-    --[[
-        Yields the thread until :Fire(...) is called. Returns the arguments that :Fire(...) provided.
-    ]]
-
-    function Signal:Wait()
-        return self._event:Wait()
-    end
-
-    --[[
-        Disconnects all connections to the signal. The signal is still useable after calling this.
-    ]]
-
-    function Signal:DisconnectAll()
-        for connection, _ in next, self._connections do
-            -- Check if connection hasnt been disconnected
-            if connection.Connected then
-                -- Disconnects connection
-                connection:Disconnect()
-            end
-        end
-
-        -- Clears all connections
-        self._connections = {}
-    end
-
-     --[[
-        Destroys the BindableEvent and disconnects all connections to the signal. The signal is still unusable after calling this.
-        Clears the table and sets the metatable to nil.
-    ]]
-
-    function Signal:Destroy()
-        if self._bindable_event then
-            -- Destroys the BindableEvent and sets the BindableEvent and Event to nil
-            self._bindable_event:Destroy()
-            self._bindable_event = nil
-            self._event = nil
-        end
-
-        -- Disconnects all connections
-        self:DisconnectAll()
-
-        -- Makes the signal unusable
-        setmetatable(self, nil)
-        table_clear(self)
-    end
+    return setmetatable({
+        Connected = true,
+        _signal = signal,
+        _connection = connection
+    }, Connection)
 end
+
+function Connection:Disconnect()
+    self._connection:Disconnect()
+    self.Connected = false
+
+    self._signal._count -= 1
+    self._signal.Active = self._signal._count > 0
+end
+
+function Signal.new()
+    local bindableEvent = newInstance("BindableEvent")
+
+    return setmetatable({
+        Active = false,
+        _bindableEvent = bindableEvent,
+        _signal = bindableEvent.Event,
+        _count = 0
+    }, Signal)
+end
+
+function Signal:Fire(...)
+    self._bindableEvent:Fire(...)
+end
+
+function Signal:Connect(callback)
+    return Connection.new(self, callback)
+end
+
+function Signal:Wait()
+    return self._signal:Wait()
+end
+
+function Signal:Destroy()
+    self._bindableEvent:Destroy()
+    self.Active = false
+    setmetatable(self, nil)
+end
+
+Signal.DisconnectAll = Signal.Destroy
+Signal.Disconnect = Signal.Destroy
 
 return Signal
